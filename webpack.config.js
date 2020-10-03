a78fe5b4568b21364af0c92fe729c3345cac373c
Huelder <huelderantuness@gmail.com>
2020-10-03 19:14:38 -0300

    add: first commit

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const dirApp = path.join(__dirname, 'app')
+const dirViews = path.join(__dirname, 'views')
+const dirAssets = path.join(__dirname, 'assets')
+const dirNode = path.join(__dirname, 'node_modules')
+const dirStyles = path.join(__dirname, 'styles')
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  },
+  entry: [path.join(dirApp, 'index.js'), path.join(dirStyles, 'index.scss')],
+  resolve: {
+    modules: [dirApp, dirAssets, dirStyles, dirNode, dirViews]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug/,
+        loader: 'pug-loader'
+      },
+      {
+        test: /\.jpe?g$|\.gif$|\.png$|\.svg$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanAfterEveryBuildPatterns: ['dist'] }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './views/index.pug'
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: './assets', to: './assets' }]
+    })
+  ]
+}
